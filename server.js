b87d489e54d7999a541f1f75766403a15b166e1e
NikolaJacques <44374495+NikolaJacques@users.noreply.github.com>
2022-04-06 14:24:24 +0200

    database connection

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const mongoose = require('mongoose')
 
 app.use(cors())
 app.use(express.static('public'))
@@ -9,10 +10,15 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
-
-
-
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
+
+// solution
+
+mongoose.connect(
+  process.env.MONGO_URI, 
+  { useNewUrlParser: true, useUnifiedTopology: true }, 
+  () => {console.log('Connection to DB successful')}, 
+  error => console.log(error.message)
+);
